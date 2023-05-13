923a371892636df524a88a082ae674a18f70686e
ruddro420 <aliruddro@gmail.com>
2023-05-14 00:59:16 +0600

    Preloader and Home Page Setup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-
-import './App.css'
+import Home from './component/Home/Home'
+import Preload from './component/Preload/Preload'
 
 function App() {
 
   return (
     <>
- <div className="preloader">
-    <div className="loading-mask"></div>
-    <div className="loading-mask"></div>
-    <div className="loading-mask"></div>
-    <div className="loading-mask"></div>
-    <div className="loading-mask"></div>
-  </div>
-
-  <div className="preview-wrapper">
-    <div className="switcher-head">
-      <span>Style Switcher</span>
-      <div className="switcher-trigger tf-tools"></div>
-    </div>
-
-    <div className="switcher-body">
-      <h4>Choose Color:</h4>
-      <ul className="color-options list-none">
-        <li className="c0" data-color="red" title="Default">Default</li>
-        <li className="c1" data-color="blue" title="Red">Red</li>
-        <li className="c2" data-color="green" title="Green">Green</li>
-        <li className="c3" data-color="yellow" title="Blue">Blue</li>
-      </ul>
-    </div>
-  </div>
+      <Preload></Preload>
+      <Home></Home>
     </>
   )
 }
---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,203 @@
+/* eslint-disable react/no-unknown-property */
+const Home = () => {
+    return (
+        <div>
+            <main className="site-wrapper">
+  <div className="pt-table">
+    <div className="pt-tablecell page-home relative">
+      <div className="overlay"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
+            <div className="page-title home text-center">
+              <img src="../images/phantom.png" alt=""/>
+              <p>A product designer from England, who focuses on interactive design &amp; A freelance designer focusing on typography &amp; clean interfaces. Also works in Google.</p>
+            </div>
+
+            <div className="hexagon-menu clear">
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="welcome.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-dial"></i>
+                    </span>
+                    <span className="title">Welcome</span>
+                  </span>
+                  <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+                      fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="about.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-profile-male"></i>
+                    </span>
+                    <span className="title">About</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="services.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-tools-2"></i>
+                    </span>
+                    <span className="title">Services</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="resume.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-tools"></i>
+                    </span>
+                    <span className="title">Resume</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="works.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-briefcase2"></i>
+                    </span>
+                    <span className="title">Works</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="testimonials.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-chat"></i>
+                    </span>
+                    <span className="title">Testimonials</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="hexagon-item">
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="hex-item">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <a href="contact.html" className="hex-content">
+                  <span className="hex-content-inner">
+                    <span className="icon">
+                      <i className="tf-envelope2"></i>
+                    </span>
+                    <span className="title">Contact</span>
+                  </span>
+                  <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path>
+                  </svg>
+                </a>
+              </div>
+            </div> 
+
+          </div> 
+
+        </div> 
+      </div> 
+
+    </div>
+  </div> 
+</main>
+        </div>
+    );
+};
+
+export default Home;
